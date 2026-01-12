fe1c30fba9da4b0235324f760f2eaff71603fe1d
KeviN-XVII <kevinquartar@gmail.com>
2026-01-12 15:53:04 +0100

    Es2

--- src/Esercizio2/Es2Int.java
+++ src/Esercizio2/Es2Int.java
@@ -2,22 +2,18 @@ package Esercizio2;
 
 import java.util.Scanner;
 
-public class Es2 {
+public class Es2Int {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
         try{
-        System.out.println("Inserisci i Km percorsi :");
-        double Km = scanner.nextDouble();
-        System.out.println("Inserisci i litri utilizzati :");
-        double litri = scanner.nextDouble();
-        if(litri == 0){
-            System.out.println("Litri non puo corrispondere a 0");
-        } else {
-            double kmlitro = Km / litri;
+            System.out.println("Inserisci i Km percorsi :");
+            int Km = scanner.nextInt();
+            System.out.println("Inserisci i litri utilizzati :");
+            int litri = scanner.nextInt();
+            int kmlitro = Km / litri;
             System.out.println("km per litro percorsi : " + kmlitro);
-        }
         }catch(Exception e){
             System.out.println("valore non valido");
         }
@@ -26,4 +22,5 @@ public class Es2 {
         }
 
     }
+
 }
